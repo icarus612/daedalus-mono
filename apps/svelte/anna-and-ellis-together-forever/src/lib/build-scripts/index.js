9eb5b8dfd45b8339806f793e3486a6d602a9d224
icarus64 <ellishogan95@gmail.com>
2024-03-02 18:23:14 -0500

    update

--- apps/svelte/anna-and-ellis-together-forever/src/lib/build-scripts/index.js
+++
@@ -1,5 +0,0 @@
-import generateBuildIndex from './generate-build-index.js';
-import generateComponentIndex from './generate-component-index.js';
-
-generateBuildIndex()
-generateComponentIndex()
--- apps/svelte/anna-and-ellis-together-forever/src/lib/components/image.svelte
+++
@@ -1,8 +0,0 @@
-
-<script>
-	export let src, alt;
-	
-	$: src = src.startsWith('/') ? src : '/' + src;
-</script>
-
- <img  {...$$props} src={'https://storage.googleapis.com/icarus64_wedding/wedding_photos' + src} alt={alt}/>
