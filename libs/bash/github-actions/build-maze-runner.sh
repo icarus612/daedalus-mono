cc84c81f19aa2b9957bc686f939e0e5f5ebf11e8
icarus64 <ellishogan95@gmail.com>
2024-08-08 22:46:17 -0400

    update

--- libs/bash/github-actions/build-maze-runner.sh
+++ libs/bash/github-actions/build-maze-runner.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Clone the destination repository
-git clone https://github.com/icarus612/maze-runner-mono.git dest-repo
+git clone https://github.com/icarus612/maze-runner-mono.git
 
 # Copy the content from all the source paths to the destination repository
-cp -R  daedalus-mono/apps/flask/maze-runner/ dest-repo/apps/flask 2>/dev/null
-cp -R  daedalus-mono/apps/next/maze-runner/ dest-repo/apps/next 2>/dev/null
+cp -R  daedalus-mono/apps/flask/maze-runner/ maze-runner-mono/apps/flask 2>/dev/null
+cp -R  daedalus-mono/apps/next/maze-runner/ maze-runner-mono/apps/next 2>/dev/null
 
-cp -R  daedalus-mono/libs/python/maze-runner/ dest-repo/libs/python 2>/dev/null
-cp -R  daedalus-mono/libs/javascript/node/maze-runner/ dest-repo/libs/javascript 2>/dev/null
-cp -R  daedalus-mono/libs/javascript/react/maze-runner/ dest-repo/libs/react-js 2>/dev/null
-cp -R  daedalus-mono/libs/javascript/solid/maze-runner/ dest-repo/libs/solid-js 2>/dev/null
+cp -R  daedalus-mono/libs/python/maze-runner/ maze-runner-mono/libs/python 2>/dev/null
+cp -R  daedalus-mono/libs/javascript/node/maze-runner/ maze-runner-mono/libs/javascript 2>/dev/null
+cp -R  daedalus-mono/libs/javascript/react/maze-runner/ maze-runner-mono/libs/react-js 2>/dev/null
+cp -R  daedalus-mono/libs/javascript/solid/maze-runner/ maze-runner-mono/libs/solid-js 2>/dev/null
 
 # Push destination repository
-cd dest-repo
+cd maze-runner-mono
+
 git config user.name github-actions
 git config user.email github-actions@github.com
 git add .
