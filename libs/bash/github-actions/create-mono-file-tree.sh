715cca9c6eae22598f3ac107c9f0c9ec1ed46dd4
icarus64 <ellishogan95@gmail.com>
2024-08-09 23:29:48 -0400

    update

--- libs/bash/github-actions/create-mono-file-tree.sh
+++ libs/bash/github-actions/create-mono-file-tree.sh
@@ -23,17 +23,17 @@ function print_tree() {
                 local subdirs=($dir/*)
                 if [ ${#subdirs[@]} -gt 0 ]; then
                     if [ $count -eq $total_dirs ]; then
-                        echo "${prefix}└── ${dir_name}"
-                        print_tree "$base_dir" "$dir" "${prefix}    \n"
+                        echo "${prefix}└── ${dir_name}\n"
+                        print_tree "$base_dir" "$dir" "${prefix}    "
                     else
-                        echo "${prefix}├── ${dir_name}"
-                        print_tree "$base_dir" "$dir" "${prefix}│   \n"
+                        echo "${prefix}├── ${dir_name}\n"
+                        print_tree "$base_dir" "$dir" "${prefix}│   "
                     fi
                 else
                     if [ $count -eq $total_dirs ]; then
-                        echo "${prefix}└── ${dir_name}"
+                        echo "${prefix}└── ${dir_name}\n"
                     else
-                        echo "${prefix}├── ${dir_name}"
+                        echo "${prefix}├── ${dir_name}\n"
                     fi
                 fi
             fi
