9702188a235ed407358675daf8e619dc8201b408
icarus64 <devicarus64@gmail.com>
2025-05-25 01:00:05 -0400

    update

--- libs/python/web-crawlers/web_crawlers/anki_scrapers/golang/packages_downloader.py
+++ libs/python/web-crawlers/web_crawlers/anki_scrapers/golang/packages_downloader.py
@@ -5,7 +5,7 @@ from os import makedirs
 import re 
 
 lib = 'builtin'
-version = '1.23.0'
+version = '1.24.0'
 def download_lib(lib=lib, version=version):
 	page =  soup(requests.get(f'https://pkg.go.dev/{lib}@go{version}').content, 'html.parser')
 	decks= {}
@@ -16,7 +16,7 @@ def download_lib(lib=lib, version=version):
 		with_options = el.find_next('div').find('pre').text
 		name = el.find("a").text.strip()
 		func_init = f'<i>{deck_name}</i>' if is_method else lib
-		r_string = re.search(f"{name}\[.*\]\(", with_options).group(0)
+		r_string = re.search(f"{name}(?:\[.*?\])?\(", with_options).group(0)
 
 		params = ', '.join([f'<i>{x.split(" ")[0]}</i>' for x in with_options.partition(r_string)[2].split(')')[0].split(', ')])
 		func = f'{func_init}.{name}({params})'
@@ -31,11 +31,16 @@ def download_lib(lib=lib, version=version):
 		else:
 			decks[deck_name] = [card]
 
+	if decks == {}:
+		print(f'No functions found for {lib} version {version}')
+		return
+	
 	try:
 		makedirs(lib)
 	except:
 		pass
-
+	
+	
 	for deck, cards in decks.items():
 		with open(f'{lib}/{deck}.txt', 'w') as file:
 			file.writelines([f'{"|".join(i)} \n' for i in cards])	
